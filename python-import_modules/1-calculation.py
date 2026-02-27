4c9a309992b148bac1878b9d28692298e30cf7fc
ben-m-m <mainaben23@gmail.com>
2026-02-27 16:56:54 +0200

    calculator_1 appears once

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-import calculator_1
+from calculator_1 import add, sub, mul, div
 
 
 if __name__ == "__main__":
     a = 10
     b = 5
-    add = calculator_1.add(a, b)
-    sub = calculator_1.sub(a, b)
-    mul = calculator_1.mul(a, b)
-    div = calculator_1.div(a, b)
+    add = add(a, b)
+    sub = sub(a, b)
+    mul = mul(a, b)
+    div = div(a, b)
     print("{} + {} = {}".format(a, b, add))
     print("{} - {} = {}".format(a, b, sub))
     print("{} * {} = {}".format(a, b, mul))
