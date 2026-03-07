3eb083eaafea1874c1d92a57f71ce4217e48ab54
ben-m-m <mainaben23@gmail.com>
2026-03-07 23:48:51 +0200

    change element at index but keep original list

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 
 def new_in_list(my_list, idx, element):
     if my_list is None:
-        return
+        return my_list
     if idx >= 0 and idx < len(my_list):
         new_list = my_list.copy()
         new_list[idx] = element
