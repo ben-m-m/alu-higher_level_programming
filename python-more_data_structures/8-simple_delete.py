b9a250c8ce17d5a418af76262fd24c72d6968343
ben-m-m <mainaben23@gmail.com>
2026-03-10 22:05:15 +0200

    delete given key in given dict

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def simple_delete(a_dictionary, key=""):
+    for k in a_dictionary:
+        if k == key:
+            del a_dictionary[key]
+            return a_dictionary
+        else:
+            return a_dictionary
