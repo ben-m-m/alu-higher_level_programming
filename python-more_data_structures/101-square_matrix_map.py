c079b83dce3841523ee25ee91e35ca8488cff480
ben-m-m <mainaben23@gmail.com>
2026-03-11 20:54:41 +0200

    map function used to square matrix 3 lines max

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-
 def square_matrix_map(matrix=[]):
-    new_matrix = list(map(lambda row: list(map(lambda i: i ** 2, row)), matrix))
-    return new_matrix
+    return new_matrix = list(map(lambda row: list(map(lambda i: i ** 2, row)), matrix))
