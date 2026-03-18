3c5e910d06c5d695b1b3f3243c598a7ca20561bb
ben-m-m <mainaben23@gmail.com>
2026-03-18 14:01:04 +0200

    square with private instance attrribute size with type and value error trap

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,13 +5,8 @@
 class Square:
     """takes size int and greater than or equal to zero"""
     def __init__(self, size=0):
-        try:
-            if not isinstance(size, int):
-                raise TypeError
-            if size < 0:
-                raise ValueError
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
