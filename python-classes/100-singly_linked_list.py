fe72cc742aeb105bee838fc970bd9e0e49268c8b
ben-m-m <mainaben23@gmail.com>
2026-03-18 22:31:10 +0200

    refactor: improve docstrings for Node and SinglyLinkedList classes

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-""""""
+"""Definessingly linked list with sorted insertion and str representation."""
 
 
 class Node:
-    """defines a node of a singly linked list"""
+    """Defines node in singly linked list with
+    data and next_node attributes."""
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
 
+    """Getter for data attribute."""
     @property
     def data(self):
         return self.__data
 
+    """Setter for data attribute with type checking."""
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
+    """Getter for next_node attribute."""
     @property
     def next_node(self):
         return self.__next_node
 
+    """Setter for next_node attribute with type checking."""
     @next_node.setter
     def next_node(self, value):
         if value is not None and not isinstance(value, Node):
@@ -30,29 +35,34 @@ class Node:
 
 
 class SinglyLinkedList:
-    """"""
+    """Defines a singly linked list with
+    sorted insertion and string representation."""
     def __init__(self):
         self.__head = None
 
+    """Inserts a new Node into the correct sorted position in the list."""
     def sorted_insert(self, value):
         new_node = Node(value)
 
-        # Case 1: empty list OR insert at beginning
+        """ If the list is empty or the new node should be the new head. """
         if self.__head is None or value < self.__head.data:
             new_node.next_node = self.__head
             self.__head = new_node
             return
 
-        # Traverse to find correct position
+        """ Traverse the list to find the
+        correct position for the new node. """
         current = self.__head
         while (current.next_node is not None and
                current.next_node.data < value):
             current = current.next_node
 
-        # Insert node
+        """ Insert the new node in its correct position. """
         new_node.next_node = current.next_node
         current.next_node = new_node
 
+    """ Returns a string representation of the
+    list with one node's data per line. """
     def __str__(self):
         result = []
         current = self.__head
